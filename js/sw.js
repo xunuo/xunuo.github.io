54e8f1b683f5cfc7d76654713316deaa787de7ae
nuo.xun <nuo.xun@alibaba-inc.com>
2016-08-10 02:22:49 +0800

    add

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,29 @@
 /**
  * Created by snow on 16/8/10.
  */
+
+const cacheUrl = [
+  '/js/index.js'
+];
+const cacheName = 'my-site-cache';
+
+// install阶段
+self.addEventListener('install', (event) => {
+  console.log('sw event: install');
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('open cache');
+      return cache.addAll(cacheUrl);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(res => {
+    if (res) {
+      console.log('match');
+      return res;
+    }
+    return fetch(event.request);
+  }));
+});
