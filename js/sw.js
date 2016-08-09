a6ab79d65bc45845d82ffbb6bd93e9d31c2ba66b
nuo.xun <nuo.xun@alibaba-inc.com>
2016-08-10 03:01:54 +0800

    add

--- js/sw.js
+++ js/sw.js
@@ -1,54 +1,63 @@
-//importScripts('/js/sw-cache-polyfill.js'); 
-//
-//var CACHE_NAME = 'my-site-cache-v2';
-//var urlsToCache = [
-//  '/js/index.js'
-//];
-//
-//self.addEventListener('install', function(event) {
-//  // Perform install steps
-//  event.waitUntil(
-//    caches.open(CACHE_NAME)
-//      .then(function(cache) {
-//        console.log('Opened cache');
-//        return cache.addAll(urlsToCache);
-//      })
-//  );
-//}); 
+importScripts('/js/sw-cache-polyfill.js'); 
 
+var CACHE_NAME = 'my-site-cache-v2';
+var urlsToCache = [
+  '/js/index.js'
+];
 
-var cacheVersion = 0;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = '/index.html';
-
-this.addEventListener('install', event => {
+// example usage:
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-          '/js/index.js',
-          offlineUrl
-      ]);
+    caches.open('demo-cache').then(function(cache) {
+      return cache.put('/', new Response("From the cache!"));
     })
   );
 });
 
-this.addEventListener('fetch', event => {
-
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-        })
-     );
-  }
-  else{
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || new Response("Nothing in the cache for this request");
+    })
+  );
 });
+
+
+
+
+
+//var cacheVersion = 0;
+//var currentCache = {
+//  offline: 'offline-cache' + cacheVersion
+//};
+//const offlineUrl = '/index.html';
+//
+//this.addEventListener('install', event => {
+//  event.waitUntil(
+//    caches.open(currentCache.offline).then(function(cache) {
+//      return cache.addAll([
+//          '/js/index.js',
+//          offlineUrl
+//      ]);
+//    })
+//  );
+//});
+//
+//this.addEventListener('fetch', event => {
+//
+//  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+//        event.respondWith(
+//          fetch(event.request.url).catch(error => {
+//              // Return the offline page
+//              return caches.match(offlineUrl);
+//        })
+//     );
+//  }
+//  else{
+//        event.respondWith(caches.match(event.request)
+//                        .then(function (response) {
+//                        return response || fetch(event.request);
+//                    })
+//            );
+//      }
+//});
