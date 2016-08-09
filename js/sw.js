d39ffa09989b9cb80c13cca0df6010a64bae0f95
nuo.xun <nuo.xun@alibaba-inc.com>
2016-08-10 02:53:39 +0800

    add

--- js/sw.js
+++ js/sw.js
@@ -1,17 +1,54 @@
-importScripts('/js/sw-cache-polyfill.js'); 
+//importScripts('/js/sw-cache-polyfill.js'); 
+//
+//var CACHE_NAME = 'my-site-cache-v2';
+//var urlsToCache = [
+//  '/js/index.js'
+//];
+//
+//self.addEventListener('install', function(event) {
+//  // Perform install steps
+//  event.waitUntil(
+//    caches.open(CACHE_NAME)
+//      .then(function(cache) {
+//        console.log('Opened cache');
+//        return cache.addAll(urlsToCache);
+//      })
+//  );
+//}); 
 
-var CACHE_NAME = 'my-site-cache-v2';
-var urlsToCache = [
-  '/js/index.js'
-];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+var cacheVersion = 0;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'index.html';
+
+this.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          './js/index.js',
+          offlineUrl
+      ]);
+    })
   );
-}); 
+});
+
+this.addEventListener('fetch', event => {
+
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+        })
+     );
+  }
+  else{
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
