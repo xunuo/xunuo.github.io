883cc4e05dbd4128218571e3416806d61ad1719a
nuo.xun <nuo.xun@alibaba-inc.com>
2016-08-10 09:30:15 +0800

    commit

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ importScripts('./sw-cache-polyfill.js');
 
 var CACHE_NAME = 'my-site-cache-v2';
 var urlsToCache = [
-  './index.js'
+  './js/index.js'
 ];
 
 // example usage:
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('demo-cache').then(function(cache) {
-      return cache.put('./index.js', new Response("From the cache!"));
+      return cache.put('./js/index.js', new Response("From the cache!"));
     })
   );
 });
